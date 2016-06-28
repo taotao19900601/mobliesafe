16c3a7d71b2ec863ea489f2c76cea5eb3f1d2d33
meitao <meitao@tigerjoys.com>
2016-06-28 16:38:18 +0800

    设置button的背景选择器

--- src/com/mt/mobliesafe/activity/Setup1Activity.java
+++ src/com/mt/mobliesafe/activity/Setup1Activity.java
@@ -1,5 +1,15 @@
 package com.mt.mobliesafe.activity;
-
+/**
+ * .9path
+ *  四条横线的说明
+ *  1.上面的黑线代表 横向拉伸
+ *  2.左边的黑线代表 纵向拉伸
+ *  3.右边的黑线代表 纵向的文字填充
+ *  4.下边的黑线代表 横向的文字填充
+ *  
+ *  
+ * 
+ */
 import com.mt.mobliesafe.R;
 
 import android.app.Activity;
@@ -10,5 +20,6 @@ public class Setup1Activity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup1);
+		
 	}
 }
