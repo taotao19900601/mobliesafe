f53bbc21cca2b2d01d2401fa4a8b806a16ec47ab
meitao <meitao@tigerjoys.com>
2016-07-01 15:46:34 +0800

    更新页面逻辑

--- src/com/mt/mobliesafe/activity/LostFindActivity.java
+++ src/com/mt/mobliesafe/activity/LostFindActivity.java
@@ -7,11 +7,14 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LostFindActivity extends Activity {
 
 	private SharedPreferences mSp;
-
+	private TextView tv_safephone;
+	private ImageView ivProtect;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,6 +22,16 @@ public class LostFindActivity extends Activity {
 		boolean configed = mSp.getBoolean("configed", false);
 		if (configed) {
 			setContentView(R.layout.activity_lost_find);
+			tv_safephone = (TextView) findViewById(R.id.tv_safe_phone);
+			ivProtect = (ImageView) findViewById(R.id.iv_protect);
+			String phone = mSp.getString("safe_phone", "");
+			boolean protect = mSp.getBoolean("protect", false);
+			if(protect){
+				ivProtect.setImageResource(R.drawable.lock);
+			}else{
+				ivProtect.setImageResource(R.drawable.unlock);
+			}
+			tv_safephone.setText(phone);
 		} else {
 			Intent intent = new Intent(LostFindActivity.this, Setup1Activity.class);
 			startActivity(intent);
--- src/com/mt/mobliesafe/recevier/BootCompleteRecevier.java
+++ src/com/mt/mobliesafe/recevier/BootCompleteRecevier.java
@@ -6,33 +6,40 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import android.util.Log;
+
 /**
  * 好多国产rom基本都监听不到开机广播
+ * 
  * @author admin
- *
+ * 
  */
-public class BootCompleteRecevier extends BroadcastReceiver{
+public class BootCompleteRecevier extends BroadcastReceiver {
 	private SharedPreferences mSp;
 	private TelephonyManager telephonyManager;
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		mSp = context.getSharedPreferences("config",Context.MODE_PRIVATE);
-		String spSim = mSp.getString("sim", null);
-		
-		if(!TextUtils.isEmpty(spSim)){
-			telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-			String simSerialNumber = telephonyManager.getSimSerialNumber();
-			if(spSim.equals(simSerialNumber)){
-				// sim卡安全
-				
-			}else{
-				//sim卡不安全
-				
+
+		mSp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
+		boolean protect = mSp.getBoolean("protect", false);
+		if (protect) {
+			String spSim = mSp.getString("sim", null);
+
+			if (!TextUtils.isEmpty(spSim)) {
+				telephonyManager = (TelephonyManager) context
+						.getSystemService(Context.TELEPHONY_SERVICE);
+				String simSerialNumber = telephonyManager.getSimSerialNumber();
+				if (spSim.equals(simSerialNumber)) {
+					// sim卡安全
+					Log.i("TAG", "sim safe");
+				} else {
+					// sim卡不安全
+					Log.i("TAG", "sim not safe");
+				}
 			}
 		}
-		
-		
+
 	}
 
 }
